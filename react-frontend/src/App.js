6c95d2f811a143f8e2ba1ddc5563776f0e842ebc
norrisb1atwit <norrisb1@wit.edu>
2024-06-05 16:30:28 -0400

    Progress on homepage layout

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <header className="App-header">
+      <button className="Dropdown-menu">Nav</button>
+        <h1 className="Homepage-title">
+          Welcome to ULearn!
+        </h1>
       </header>
+      <aside className = "Sidebar-nav">
+              <p>Python Modules</p>
+              <p>Java Modules</p>
+              <p>C++ Modules</p>
+              <p className="Credit">About</p>
+      </aside>
+      <section className="Mainpage-body">
+
+      </section>
     </div>
   );
 }
