25b7028ad192a65df8f60aa1c32c6a9ee51dc7f3
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-06-12 23:25:30 -0400

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,23 @@
 //import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './Styles.css';
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Introduction from "./pages/Introduction";
-import Navbar from "./Controls/Navbar";
+import Home from './pages/Home';
+import Introduction from './pages/Introduction';
+import Navbar from './Controls/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/introduction" element={<Introduction />} />
-        {/* Add more routes as needed */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
 export default App;
+
 
