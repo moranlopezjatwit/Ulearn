d30e276423ee7574d5c66ee7e25a353407b1eae2
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-06-13 14:09:20 -0400

    Update CppModules.js

--- react-frontend/src/pages/modules/CppModules.js
+++ react-frontend/src/pages/modules/CppModules.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { LanguageContext } from '../LanguageContext';
+import { LanguageContext } from '../../LanguageContext'; // Corrected path
 
 function CppModules() {
   const { languages } = useContext(LanguageContext);
