613e8d8ece4292beb45bbfa0a541f61d9704a4a7
norrisb1atwit <norrisb1@wit.edu>
2024-06-17 17:43:47 -0400

    Began setup for Landing page

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-       <h1 class="Section-header">What is ULearn?</h1>
+    <div class="Landing-page">
+       <h1 class="Landing-header">Welcome to ULearn</h1>
        
               <p classname="Paragraph-box">ULearn is a web-based platform designed to offer personalized educational programming resources to individuals of all skill levels.</p>
        
