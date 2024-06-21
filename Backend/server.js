8016b2bf491250f021a310db046cb2dae8939008
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-06-20 20:16:30 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
 
-// Connect Database
+// Connect to MongoDB
 connectDB();
 
-// Init Middleware
-app.use(bodyParser.json());
+// Middleware
+app.use(express.json());
 
-// Define Routes
+// Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/recommendations', require('./routes/recommendation'));
+app.use('/api', require('./routes/protectedRoute'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
