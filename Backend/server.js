1f2347b0db97f1cbbb1ac936ed09c00cc52aec07
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-06-25 18:38:18 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -11,7 +12,11 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api', require('./routes/protectedRoute'));
+
+// Example of a protected route
+app.get('/api/protected', auth, (req, res) => {
+  res.send('This is a protected route');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
