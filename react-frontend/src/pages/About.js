80fbdba473e685d5e916f2a1ce7c09a23d84269b
nsimms35 <71151185+nsimms35@users.noreply.github.com>
2024-07-02 12:10:38 -0400

    Update About.js
    Added more description on the about page(Coding this directly from Git)

--- react-frontend/src/pages/About.js
+++ react-frontend/src/pages/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 export default function About() {
     return (<>
         <div>
-        
+        <p>The demand for personalized educational resources in the field of computer science is on the rise, reflecting the
+        broader trend towards online learning. ULearn aims to address this need by providing a platform that utilizes 
+        predictive algorithms to deliver tailored learning materials to users based on their coding interests and 
+        proficiency levels.</p>
+        <br/>
         <p>ULearn was created by Juan Moran Lopez, Nathaniel Simms, and Brandon Norris.</p>
         <br/>
         <p>Credits for any imagery, media, and references used will eventually go here.</p>
