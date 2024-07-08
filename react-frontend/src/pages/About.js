d8a4d2663355c58e42987b6e495322c6b7dc6859
nsimms35 <simmsn@wit.edu>
2024-07-07 23:46:50 -0400

    Updated about page

--- react-frontend/src/pages/About.js
+++ react-frontend/src/pages/About.js
@@ -2,14 +2,27 @@ import React from 'react';
 export default function About() {
     return (<>
         <div id="App-container">
+            <h1>About</h1>
+
         <p>The demand for personalized educational resources in the field of computer science is on the rise, reflecting the
-        broader trend towards online learning. ULearn aims to address this need by providing a platform that utilizes 
+        broader trend towards online learning.
+        <br />
+        <br />
+        ULearn aims to address this need by providing a platform that utilizes
         predictive algorithms to deliver tailored learning materials to users based on their coding interests and 
-        proficiency levels.</p>
-        <br/>
-        <p>ULearn was created by Juan Moran Lopez, Nathaniel Simms, and Brandon Norris.</p>
-        <br/>
-        <p>Credits for any imagery, media, and references used will eventually go here.</p>
+                proficiency levels.
+                <br />
+                <br />
+                ULearn was created by Juan Moran Lopez, Nathaniel Simms, and Brandon Norris.
+                <br />
+                <br />
+                Credits for any imagery, media, and references used will eventually go here.
+
+
+            </p>
+        
+
+        
         </div>
     </>
     );
