dc3560bdef8f04cc0cf72894c67355d3426cb2f5
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-11 19:34:59 -0400

    Update Register.js

--- react-frontend/src/pages/Register.js
+++ react-frontend/src/pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { register } from './api';
+import { register } from '../api';
 
 const Register = () => {
   const [formData, setFormData] = useState({ name: '', email: '', password: '' });
