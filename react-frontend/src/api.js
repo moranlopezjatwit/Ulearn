8b8533c0f20822176720f63885c0b670243be4c1
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-11 19:32:57 -0400

    Update api.js

--- react-frontend/src/api.js
+++ react-frontend/src/api.js
@@ -16,3 +16,4 @@ API.interceptors.request.use((config) => {
 export const register = (formData) => API.post('/auth/register', formData);
 export const login = (formData) => API.post('/auth/login', formData);
 export const getProtectedData = () => API.get('/protected');
+
