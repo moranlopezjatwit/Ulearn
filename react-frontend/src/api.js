a3c43fbc4b528761741d46b5566ba52fc3b8265a
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-11 16:54:39 -0400

    Create api.js

---
+++ react-frontend/src/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: '/api',
+});
+
+// Interceptor to add token to headers
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const register = (formData) => API.post('/auth/register', formData);
+export const login = (formData) => API.post('/auth/login', formData);
+export const getProtectedData = () => API.get('/protected');
