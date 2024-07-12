7bf2a7d11ed5d334b1d2512dfb3a56af0e902251
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-11 20:46:07 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const authRoutes = require('./routes/auth');
 
 const app = express();
