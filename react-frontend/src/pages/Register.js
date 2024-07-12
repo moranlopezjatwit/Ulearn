e3f4c2586851c8b2ddc5ec5d72803bbe77c9b006
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-12 16:40:43 -0400

    Update Register.js

--- react-frontend/src/pages/Register.js
+++ react-frontend/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { register } from '../api';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
   const [username, setUsername] = useState('');
