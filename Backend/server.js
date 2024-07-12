cba73b8f0589ca2ad34cc9fde44f40bbffddea20
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-11 20:26:49 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
-const errorHandler = require('./utils/errorHandler');
+const authRoutes = require('./routes/auth');
 
 const app = express();
+
+// Use CORS middleware
+app.use(cors());
+
+app.use(express.json());
 
 // Connect to MongoDB
 connectDB();
 
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api', require('./routes/protectedRoute'));
-
-// Error handling middleware
-app.use(errorHandler);
+// Define routes
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
