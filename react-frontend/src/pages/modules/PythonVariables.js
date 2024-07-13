61b12e35682c2b2cc1e7f3733f4a291ae06639a7
norrisb1atwit <norrisb1@wit.edu>
2024-07-13 17:42:05 -0400

    Added content to python variables lesson module

--- react-frontend/src/pages/modules/PythonVariables.js
+++ react-frontend/src/pages/modules/PythonVariables.js
@@ -7,7 +7,17 @@ export default function PythonVariables() {
     return (
         <div id="App-container">
             <PythonSidenav />
-            Python Variables
-        </div>
+            <div>
+            <h1 class="Section-header">Variables</h1>
+            <p class="Section-content">For any programming language, variables are one of the fundamental building blocks with which software
+                    is built. A variable can be thought of as a method of information storage that is associated with a name. You can choose a value 
+                    for the variable to remember during its creation, or you can leave it empty until it's needed. additionally, variables can be used 
+                    to manipulate the information that they store, or they can overwrite it with new information if needed.
+                </p>
+                <br />
+                <p>Look at the example image below to see what variable creation looks like in python:</p>
+
+            </div>
+       </div>
     );
 }
