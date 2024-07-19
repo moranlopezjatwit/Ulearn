488c4bb35900c629d61fb387cd17dc571edfb20f
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-19 10:06:21 -0400

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -34,58 +34,52 @@ import Protected from './pages/Protected';
 import MyComponent from './components/MyComponent';
 
 export default function App() {
+  const { setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const user = jwtDecode(token);
+      setUser({ username: user.username });
+    }
+  }, [setUser]);
+
   return (
     <div>
       <title>ULearn</title>
       <UserProvider>
         <LanguageProvider>
-          <Router> 
-            <AppRoutes />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/Home" element={<><Navbar /><Home /><Footer /></>} />
+              <Route path="/Introduction" element={<><Navbar /><Introduction /><Footer /></>} />
+              <Route path="/About" element={<><Navbar /><About /><Footer /></>} />
+
+              <Route path="/Python-Modules" element={<><Navbar /><PythonModules /><Footer /></>} />
+              <Route path="/Python-Variables" element={<><Navbar /><PythonVariables /><Footer /></>} />
+              <Route path="/Python-Loops" element={<><Navbar /><PythonLoops /><Footer /></>} />
+              <Route path="/Python-Functions" element={<><Navbar /><PythonFunctions /><Footer /></>} />
+
+              <Route path="/Java-Modules" element={<><Navbar /><JavaModules /><Footer /></>} />
+              <Route path="/Java-Variables" element={<><Navbar /><JavaVariables /><Footer /></>} />
+              <Route path="/Java-Loops" element={<><Navbar /><JavaLoops /><Footer /></>} />
+              <Route path="/Java-Functions" element={<><Navbar /><JavaFunctions /><Footer /></>} />
+
+              <Route path="/Cpp-Modules" element={<><Navbar /><CppModules /><Footer /></>} />
+              <Route path="/Cpp-Variables" element={<><Navbar /><CppVariables /><Footer /></>} />
+              <Route path="/Cpp-Loops" element={<><Navbar /><CppLoops /><Footer /></>} />
+              <Route path="/Cpp-Functions" element={<><Navbar /><CppFunctions /><Footer /></>} />
+
+              <Route path="/Signup" element={<><Navbar /><Signup /><Footer /></>} />
+              <Route path="/Register" element={<><Navbar /><Register /></>} />
+              <Route path="/login" element={<><Navbar /><Login /><Footer /></>} />
+              <Route path="/protected" element={<><Navbar /><Protected /><Footer /></>} />
+              <Route path="/my-component" element={<><Navbar /><MyComponent /><Footer /></>} />
+            </Routes>
           </Router>
         </LanguageProvider>
       </UserProvider>
     </div>
   );
 }
-
-const AppRoutes = () => {
-  const { setUser } = useContext(UserContext);
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const user = jwt.decode(token);
-      setUser({ username: user.username });
-    }
-  }, [setUser]);
-
-  return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/Home" element={<><Navbar /><Home /><Footer /></>} />
-      <Route path="/Introduction" element={<><Navbar /><Introduction /><Footer /></>} />
-      <Route path="/About" element={<><Navbar /><About /><Footer /></>} />
-
-      <Route path="/Python-Modules" element={<><Navbar /><PythonModules /><Footer /></>} />
-      <Route path="/Python-Variables" element={<><Navbar /><PythonVariables /><Footer /></>} />
-      <Route path="/Python-Loops" element={<><Navbar /><PythonLoops /><Footer /></>} />
-      <Route path="/Python-Functions" element={<><Navbar /><PythonFunctions /><Footer /></>} />
-
-      <Route path="/Java-Modules" element={<><Navbar /><JavaModules /><Footer /></>} />
-      <Route path="/Java-Variables" element={<><Navbar /><JavaVariables /><Footer /></>} />
-      <Route path="/Java-Loops" element={<><Navbar /><JavaLoops /><Footer /></>} />
-      <Route path="/Java-Functions" element={<><Navbar /><JavaFunctions /><Footer /></>} />
-
-      <Route path="/Cpp-Modules" element={<><Navbar /><CppModules /><Footer /></>} />
-      <Route path="/Cpp-Variables" element={<><Navbar /><CppVariables /><Footer /></>} />
-      <Route path="/Cpp-Loops" element={<><Navbar /><CppLoops /><Footer /></>} />
-      <Route path="/Cpp-Functions" element={<><Navbar /><CppFunctions /><Footer /></>} />
-
-      <Route path="/Signup" element={<><Navbar /><Signup /><Footer /></>} />
-      <Route path="/Register" element={<><Navbar /><Register /></>} />
-      <Route path="/login" element={<><Navbar /><Login /><Footer /></>} />
-      <Route path="/protected" element={<><Navbar /><Protected /><Footer /></>} />
-      <Route path="/my-component" element={<><Navbar /><MyComponent /><Footer /></>} />
-    </Routes>
-  );
-};
