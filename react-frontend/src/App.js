383f87ba5dc675a1e0a4eeee61fca4420c37da91
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-19 09:41:59 -0400

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './Styles.css';
