36e7cb67b8585099b0d15f7ae7bb551e0ddfe9c1
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-19 09:08:20 -0400

    Update Register.js

--- react-frontend/src/pages/Register.js
+++ react-frontend/src/pages/Register.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { register } from '../api';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 
 const Register = () => {
