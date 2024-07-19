d392ee35e214fccd3359e7b669b7f0ae95ffc257
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-19 10:09:42 -0400

    Update UserContext.js

--- react-frontend/src/context/UserContext.js
+++ react-frontend/src/context/UserContext.js
@@ -1,10 +1,18 @@
-import React, { createContext, useState } from 'react';
-import jwtDecode from 'jwt-decode';  // Ensure this import is here
+import React, { createContext, useState, useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedUser = jwtDecode(token);
+      setUser(decodedUser);
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -12,4 +20,3 @@ export const UserProvider = ({ children }) => {
     </UserContext.Provider>
   );
 };
-
