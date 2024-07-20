f7c322216ab55b089c46487c5ff7b9261cb93659
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-19 21:28:29 -0400

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,4 @@
 // src/App.js
-
 import React, { useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -48,7 +47,9 @@ function App() {
 
   return (
     <div>
-      <title>ULearn</title>
+      <head>
+        <title>ULearn</title>
+      </head>
       <UserProvider>
         <LanguageProvider>
           <Router>
@@ -57,22 +58,18 @@ function App() {
               <Route path="/Home" element={<><Navbar /><Home /><Footer /></>} />
               <Route path="/Introduction" element={<><Navbar /><Introduction /><Footer /></>} />
               <Route path="/About" element={<><Navbar /><About /><Footer /></>} />
-
               <Route path="/Python-Modules" element={<><Navbar /><PythonModules /><Footer /></>} />
               <Route path="/Python-Variables" element={<><Navbar /><PythonVariables /><Footer /></>} />
               <Route path="/Python-Loops" element={<><Navbar /><PythonLoops /><Footer /></>} />
               <Route path="/Python-Functions" element={<><Navbar /><PythonFunctions /><Footer /></>} />
-
               <Route path="/Java-Modules" element={<><Navbar /><JavaModules /><Footer /></>} />
               <Route path="/Java-Variables" element={<><Navbar /><JavaVariables /><Footer /></>} />
               <Route path="/Java-Loops" element={<><Navbar /><JavaLoops /><Footer /></>} />
               <Route path="/Java-Functions" element={<><Navbar /><JavaFunctions /><Footer /></>} />
-
               <Route path="/Cpp-Modules" element={<><Navbar /><CppModules /><Footer /></>} />
               <Route path="/Cpp-Variables" element={<><Navbar /><CppVariables /><Footer /></>} />
               <Route path="/Cpp-Loops" element={<><Navbar /><CppLoops /><Footer /></>} />
               <Route path="/Cpp-Functions" element={<><Navbar /><CppFunctions /><Footer /></>} />
-
               <Route path="/Signup" element={<><Navbar /><Signup /><Footer /></>} />
               <Route path="/Register" element={<><Navbar /><Register /></>} />
               <Route path="/login" element={<><Navbar /><Login /><Footer /></>} />
