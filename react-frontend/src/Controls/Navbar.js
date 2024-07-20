0b239d75589fe7a400ec47cbb6e3d734c36d9531
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-19 21:23:10 -0400

    Update Navbar.js

--- react-frontend/src/Controls/Navbar.js
+++ react-frontend/src/Controls/Navbar.js
@@ -1,5 +1,4 @@
 // src/Controls/Navbar.js
-
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
@@ -44,7 +43,7 @@ const Navbar = () => {
         <li><Link to="/About">About</Link></li>
         {user ? (
           <>
-            <li><span>Hello, {user.username || 'User'}</span></li>
+            <li><span>Hello, {user.username}</span></li>
             <li><button onClick={handleLogout}>Logout</button></li>
           </>
         ) : (
