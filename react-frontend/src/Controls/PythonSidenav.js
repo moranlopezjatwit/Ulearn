f91f1347ea67568e1fc6e83763c8807757b64af6
nsimms35 <simmsn@wit.edu>
2024-07-23 11:49:00 -0400

    Highlighted the module page where the user is in

--- react-frontend/src/Controls/PythonSidenav.js
+++ react-frontend/src/Controls/PythonSidenav.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../Styles.css';
 
 export default function PythonSidenav() {
+    const location = useLocation();
+
     return (
         <div className="Side-nav-align">
             <nav className="Side-nav">
-
                 <div className="Side-header">
                     <p>Lessons</p>
-                    <a href="/Introduction"><button class="Side-back">Return</button></a>
+                    <a href="/Introduction"><button className="Side-back">Return</button></a>
                 </div>
                 <ul>
-                    <li><Link to="/Python-Modules">Introduction</Link></li>
-                    <li><Link to="/Python-Variables">Variables</Link></li>
-                    <li><Link to="/Python-Loops">Conditionals/Loops</Link></li>
-                    <li><Link to="/Python-Functions">Functions</Link></li>
-
+                    <li>
+                        <Link
+                            to="/Python-Modules"
+                            className={location.pathname === '/Python-Modules' ? 'active' : ''}
+                        >
+                            Introduction
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/Python-Variables"
+                            className={location.pathname === '/Python-Variables' ? 'active' : ''}
+                        >
+                            Variables
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/Python-Loops"
+                            className={location.pathname === '/Python-Loops' ? 'active' : ''}
+                        >
+                            Conditionals/Loops
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/Python-Functions"
+                            className={location.pathname === '/Python-Functions' ? 'active' : ''}
+                        >
+                            Functions
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </div>
