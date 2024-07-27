a760bc64884fc99337a2c0fc7e584318c69e4c4e
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-26 23:27:05 -0400

    Update server.js

--- react-frontend/server.js
+++ react-frontend/server.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/auth'); // Make sure the path is correct
-const bodyParser = require('body-parser'); // Middleware to parse request bodies
+const authRoutes = require('./routes/auth');
+const bodyParser = require('body-parser');
+
+// Helper function to import ES modules dynamically
+const importESM = async (module) => (await import(module)).default;
 
 const app = express();
-app.use(cors()); // Enable CORS for all routes
-app.use(bodyParser.json()); // Parse JSON request bodies
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
 
-// Endpoint to get local IP address
 app.get('/getLocalIp', async (req, res) => {
+  const internalIp = await importESM('internal-ip');
   const ip = await internalIp.v4();
   res.json({ ip });
 });
 
-// Authentication routes
-app.use('/api/auth', authRoutes);
-
-// Add any additional middleware or routes here
-// Example: app.use('/api/someRoute', someRouteHandler);
-
-// Your other routes and middleware
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Route not found' });
 });
