ed990fd72f5d52c1fa6b700c685ac308968ff3f2
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-07-26 23:29:13 -0400

    Update QRCodeGenerator.js

--- react-frontend/src/components/QRCodeGenerator.js
+++ react-frontend/src/components/QRCodeGenerator.js
@@ -1,8 +1,27 @@
-// src/components/QRCodeGenerator.js
-import React from 'react';
-import { QRCodeCanvas } from 'qrcode.react';
+import React, { useState, useEffect } from 'react';
 
-const QRCodeGenerator = ({ url }) => {
+const QRCodeComponent = () => {
+  const [localIp, setLocalIp] = useState('');
+
+  const fetchLocalIp = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/getLocalIp');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setLocalIp(data.ip);
+    } catch (error) {
+      console.error('Failed to fetch', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLocalIp();
+  }, []);
+
+  const url = `http://${localIp}:3000/Register`;
+
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>
       <QRCodeCanvas value={url} />
@@ -11,4 +30,4 @@ const QRCodeGenerator = ({ url }) => {
   );
 };
 
-export default QRCodeGenerator;
+export default QRCodeComponent;
