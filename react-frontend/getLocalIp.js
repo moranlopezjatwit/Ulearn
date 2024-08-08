d38c67199ab26f65f0b27defc966f5a7e5463ca9
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-08-07 20:36:06 -0400

    Update getLocalIp.js

--- react-frontend/getLocalIp.js
+++ react-frontend/getLocalIp.js
@@ -1,10 +1,1 @@
-// getLocalIp.js
-const internalIp = require('internal-ip');
 
-const getLocalIp = async () => {
-  const ip = await internalIp.v4();
-  console.log(ip);
-  return ip;
-};
-
-getLocalIp();
