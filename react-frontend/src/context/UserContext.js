2104d71a60f6a6390398d0b7c8835f593401304d
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2024-08-08 10:22:42 -0400

    Update UserContext.js

--- react-frontend/src/context/UserContext.js
+++ react-frontend/src/context/UserContext.js
@@ -1,4 +1,3 @@
-// context/UserContext.js
 import React, { createContext, useState, useEffect } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
@@ -6,35 +5,34 @@ import axios from 'axios';
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [progress, setProgress] = useState([]);
+  const [user, setUser] = useState(null);
+  const [progress, setProgress] = useState([]);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            const decodedUser = jwtDecode(token);
-            setUser(decodedUser);
-            fetchProgress(decodedUser._id);
-        }
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedUser = jwtDecode(token);
+      setUser(decodedUser);
+      fetchProgress(decodedUser._id);
+    }
+  }, []);
 
-    const fetchProgress = async (userId) => {
-        try {
-            const res = await axios.get('http://localhost:5000/api/progress/user-progress', {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('token')}`,
-                },
-            });
-            setProgress(res.data);
-        } catch (error) {
-            console.error('Error fetching progress:', error);
-        }
-    };
+  const fetchProgress = async (userId) => {
+    try {
+      const res = await axios.get(`http://localhost:5000/api/progress/user-progress/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      setProgress(res.data);
+    } catch (error) {
+      console.error('Error fetching progress:', error);
+    }
+  };
 
-    return (
-        <UserContext.Provider value={{ user, setUser, progress, setProgress }}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{ user, setUser, progress, setProgress }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
-
